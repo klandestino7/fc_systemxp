199c20a3f14906f336b01fde9c3b13f240f60e15
klandestino7 <cleidson-almeida@hotmail.com>
2020-02-11 21:20:54 -0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,8 +1,5 @@
 Config = {}
 
-Config.time = 10 -- Tempo em MINUTOS para o salário VIP cair na conta
+Config.time = 20 -- TIME IN MINUTES
  
-Config.xp3 = 50 -- Quantidade de XP para vip3
-Config.xp2 = 80 -- Quantidade de XP para vip2
-Config.xp1 = 100 -- Quantidade de XP para vip1
-Config.xpuser = 20 -- Quantidade de XP para user
+Config.xpuser = 20 -- XP
