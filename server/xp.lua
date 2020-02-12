b1d4700168be2b85b5e6babd6373e9dd4051e9a5
klandestino7 <cleidson-almeida@hotmail.com>
2020-02-11 21:14:46 -0300

    Update xp.lua

--- server/xp.lua
+++ server/xp.lua
@@ -1,90 +1,116 @@
 
 RegisterServerEvent("fcxp:systemxp")
-AddEventHandler('fcxp:systemxp', function (id, count, cb)
-
+AddEventHandler('fcxp:systemxp', function (count)
+    local countxp = count    
 	local _source = tonumber(source)
 	TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
-
 	print("XP Distribuido com Sucesso.")
-
 	if (user.getGroup() == "admin" or user.getGroup() == "superadmin") then
-
 		local _source = tonumber(source)
 		TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
-		--print(user.getIdentifier())
 			if user == nil then
-				--TODO Temporary command Feeback	
 				print("Admin command Feedback: this user doesnt exist")
 			else
-				user.addXP(tonumber(Config.xpuser))
+				user.addXP(tonumber(countxp))
 			end
 		end)
 	elseif user.getGroup() == "user" then
 		local _source = tonumber(source)
 		TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
-		--print(user.getIdentifier())
 			if user == nil then
-				--TODO Temporary command Feeback	
 				print("Admin command Feedback: this user doesnt exist")
 			else
-				user.addXP(tonumber(Config.xpuser))
-
+				user.addXP(tonumber(countxp))
 			end
 		end)
 	elseif user.getGroup() == "vip1" then
 		local _source = tonumber(source)
 		TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
-		--print(user.getIdentifier())
 			if user == nil then
-				--TODO Temporary command Feeback	
 				print("Admin command Feedback: this user doesnt exist")
 			else
-				user.addXP(tonumber(Config.xpuser))
-
+				user.addXP(tonumber(countxp*1.10))
 			end
 		end)
 	elseif user.getGroup() == "vip2" then
 		local _source = tonumber(source)
 		TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
-		--print(user.getIdentifier())
 			if user == nil then
-				--TODO Temporary command Feeback	
 				print("Admin command Feedback: this user doesnt exist")
 			else
-				user.addXP(tonumber(Config.xpuser))
+				user.addXP(tonumber(countxp*1.20))
 			end
 		end)
 	elseif user.getGroup() == "vip3" then
 		local _source = tonumber(source)
 		TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
-		--print(user.getIdentifier())
 			if user == nil then
-				--TODO Temporary command Feeback	
 				print("Admin command Feedback: this user doesnt exist")
 			else
-				user.addXP(tonumber(Config.xpuser))
+				user.addXP(tonumber(countxp*1.30))
 			end
 		end)
 	end
 end)
-end) 
+end)
 
--- Loss of 20% of XP if you die   (call the event in client side).
 RegisterServerEvent("fcxp:xpremovedeath")
-AddEventHandler('fcxp:xpremovedeath', function (id, count, cb)
-	local _source = tonumber(source)
+AddEventHandler('fcxp:xpremovedeath', function(src)
+	local _source = source
+	TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
+	if (user.getGroup() == "admin" or user.getGroup() == "superadmin") then
 		TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
-
 		local xpBalance = user.getXP()
-		local xpperda = tonumber(xpBalance*0.8)
+		local xpperda = tonumber(xpBalance)
+			if user == nil then
+				print("Admin command Feedback: this user doesnt exist")
+			else
+				user.setXP(tonumber(xpperda*0.80))
+			end
+		end)
+	elseif user.getGroup() == "user" then	
+		TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
+		local xpBalance = user.getXP()
+		local xpperda = tonumber(xpBalance)
+			if user == nil then
+				print("Admin command Feedback: this user doesnt exist")
+			else
+				user.setXP(tonumber(xpperda*0.80))
+			end
+		end)
+	elseif user.getGroup() == "vip1" then	
+		TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
+		local xpBalance = user.getXP()
+		local xpperda = tonumber(xpBalance)
 		--print(user.getIdentifier())
 			if user == nil then
 				print("Admin command Feedback: this user doesnt exist")
 			else
-				user.setXP(tonumber(xpperda))
+				user.setXP(tonumber(xpperda*0.85))
 			end
 		end)
+	elseif user.getGroup() == "vip2" then
+		TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
+		local xpBalance = user.getXP()
+		local xpperda = tonumber(xpBalance)
+		--print(user.getIdentifier())
+			if user == nil then
+				print("Admin command Feedback: this user doesnt exist")
+			else
+				user.setXP(tonumber(xpperda*0.90))
+			end
+		end)
+	elseif user.getGroup() == "vip3" then
+		TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
+		local xpBalance = user.getXP()
+		local xpperda = tonumber(xpBalance)
+		--print(user.getIdentifier())
+			if user == nil then
+				print("Admin command Feedback: this user doesnt exist")
+			else
+				user.setXP(tonumber(xpperda*0.95))
+			end
+		end)
+	end
 end)
-
-
-
+end)
